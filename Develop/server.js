a3b61ce597b29b24b20e50893b8a720642463894
Kole Dallam <kolejasonD@gmail.com>
2023-05-19 17:37:14 -0600

    starter code

---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = require('./routes');
+const { sequelize } = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+(async () => {
+  try {
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  } catch (error) {
+    console.error('Failed to start the app:', error);
+  }
+})();
