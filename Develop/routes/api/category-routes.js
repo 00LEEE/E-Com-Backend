1752212fb87161a70b8c63ff91ab83887467f6a1
Kole Dallam <kolejasonD@gmail.com>
2023-05-20 16:31:47 -0600

    fixing route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -63,8 +63,10 @@ router.delete('/:id', async (req, res) => {
     }
     res.status(200).json({ message: 'Category deleted successfully.' });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
+
 
 module.exports = router;
